fa7be96cced141c07683b41ad9068fe7d5a47312
KhalilHamwi <khalilhamwi78@gmail.com>
2023-05-18 18:31:15 +0300

    end 2

--- device/admin.py
+++ device/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from user.models import User
+from . import models
+
+
 # Register your models here.
 
+@admin.register(models.Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['api_key', 'ultrasonic_left_value', 'ultrasonic_right_value', 'gps_lat', 'gps_lng', 'gps_h',
+                    'gps_m',
+                    'gps_s', 'membership']
--- device/models.py
+++ device/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+
+
+class Device(models.Model):
+    SAVE = 'S'
+    DANGER = 'D'
+    OUT_OF_AREA = 'O'
+
+    MEMBERSHIP_CHOICES = [
+        (SAVE, '0'),
+        (DANGER, '1'),
+        (OUT_OF_AREA, '2'),
+    ]
+
+    api_key = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    ultrasonic_left_value = models.FloatField()
+    ultrasonic_right_value = models.FloatField()
+    gps_lat = models.FloatField()
+    gps_lng = models.FloatField()
+    gps_h = models.FloatField()
+    gps_m = models.FloatField()
+    gps_s = models.FloatField()
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=SAVE)
