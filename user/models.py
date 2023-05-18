e0ce2d620fdaa00c9d0e468a46024421a3ffcf12
KhalilHamwi <khalilhamwi78@gmail.com>
2023-05-18 17:58:25 +0300

    end 1

---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
